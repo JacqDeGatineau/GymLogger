b3c900f6ec3e98f107cdc25afe58fb29d775252d
Henrik Heinonen <henrik.q.heinonen@helsinki.fi>
2025-09-06 17:12:26 +0300

    add index.html

--- app.py
+++ app.py
@@ -8,4 +8,12 @@ def index():
 
 @app.route("/page1")
 def page1():
-    return
+    content = ""
+    for i in range(1, 100):
+        content += str(i) + " "
+    return content
+
+@app.route("/page/<int:page_id>")
+def page2(page_id):
+    return "Tämä on sivu " + str(page_id)
+
